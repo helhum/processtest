be1fe8fa4d68ec8e80efdf931c3c77a9bbf1772e
Helmut Hummel <helmut.hummel@typo3.org>
2014-12-15 23:21:52 +0100

    clearify messages

--- test.php
+++ test.php
@@ -6,21 +6,25 @@ mkdir('test1');
 chdir('test1');
 mkdir('test2');
 
-echo 'No cwd:' . LF;
+
+
+echo 'getcwd(): ' . getcwd() . LF;
+echo LF;
+echo 'Passed cwd: null' . LF;
 $process1 = new \Symfony\Component\Process\Process('pwd');
 $process1->run();
 echo 'Output: ' . $process1->getOutput() . LF;
 //echo 'Error Output: ' . $process1->getErrorOutput() . LF;
 echo LF;
 
-echo 'Relative path:' . LF;
+echo 'Passed cwd: "test2/"' . LF;
 $process2 = new \Symfony\Component\Process\Process('pwd', 'test2/');
 $process2->run();
 echo 'Output: ' . $process2->getOutput() . LF;
 //echo 'Error Output: ' . $process2->getErrorOutput() . LF;
 echo LF;
 
-echo 'Absolute path:' . LF;
+echo sprintf('Passed cwd: "%s"' . LF, realpath('test2/'));
 $process3 = new \Symfony\Component\Process\Process('pwd', realpath('test2/'));
 $process3->run();
 echo 'Output: ' . $process2->getOutput() . LF;
