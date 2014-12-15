2e04aa688de6e652d318a6a926efc147ed4c78d5
Helmut Hummel <helmut.hummel@typo3.org>
2014-12-15 22:46:43 +0100

    further examples

--- test.php
+++ test.php
@@ -1,20 +1,29 @@
 <?php
-
+define('LF', chr(10));
 require __DIR__ . '/vendor/autoload.php';
 
 mkdir('test1');
 chdir('test1');
 mkdir('test2');
 
-$p = new \Symfony\Component\Process\Process('pwd');
-$p->run();
-echo 'Output: ' . $p->getOutput() . chr(10);
-echo 'Error Output: ' . $p->getErrorOutput() . chr(10);
-echo chr(10);
+echo 'No cwd:' . LF;
+$process1 = new \Symfony\Component\Process\Process('pwd');
+$process1->run();
+echo 'Output: ' . $p->getOutput() . LF;
+echo 'Error Output: ' . $p->getErrorOutput() . LF;
+echo LF;
 
-$p = new \Symfony\Component\Process\Process('pwd', 'test2/');
-$p->run();
-echo 'Output: ' . $p->getOutput() . chr(10);
-echo 'Error Output: ' . $p->getErrorOutput() . chr(10);
-echo chr(10);
+echo 'Relative path:' . LF;
+$process2 = new \Symfony\Component\Process\Process('pwd', 'test2/');
+$process2->run();
+echo 'Output: ' . $p->getOutput() . LF;
+echo 'Error Output: ' . $p->getErrorOutput() . LF;
+echo LF;
+
+echo 'Absolute path:' . LF;
+$process3 = new \Symfony\Component\Process\Process('pwd', realpath('test2/'));
+$process3->run();
+echo 'Output: ' . $p->getOutput() . LF;
+echo 'Error Output: ' . $p->getErrorOutput() . LF;
+echo LF;
 
